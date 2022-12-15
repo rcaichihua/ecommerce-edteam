549d05e2a8d520144d87ae72d50f036a42132b27
Roger davy Caichihua vergara <rcaichihua@gmail.com>
2022-12-15 04:42:32 -0500

    Se agrego configuracion de tailwind.

--- src/components/molecules/header/Logo.jsx
+++ src/components/molecules/header/Logo.jsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Logo = () => {
   return (
-    <div>
-      <img src="https://img.freepik.com/vector-gratis/logo-abstracto-empresa-tecnologica-degradado_52683-9702.jpg?size=338&ext=jpg&ga=GA1.2.92368367.1670741880" alt="Logo CaiTech" />
+    <div className='logo flex'>
+      <Link to="/">
+        <div>
+          <img src="https://img.freepik.com/vector-gratis/logo-abstracto-empresa-tecnologica-degradado_52683-9702.jpg?size=338&ext=jpg&ga=GA1.2.92368367.1670741880"
+            alt="Logo CaiTech" />
+        </div>
+      </Link>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        200: '78rem'
+      },
+      width: {
+        20: '6rem'
+      }
+    },
   },
   plugins: [],
-}
+};
